35a611c16ff7892daf2fcfa0f3956cf3b6f8c2f8
PRANJAL-SHARMA9 <97629914+PRANJAL-SHARMA9@users.noreply.github.com>
2023-01-23 13:01:15 +0000

    "Bottom & onboard"

--- taza/App.js
+++ taza/App.js
@@ -8,6 +8,10 @@ import NotificationScreen from './Screen/NotificationScreen';
 import SearchScreen from './Screen/SearchScreen'; 
 import Onboarding from './Screen/Onboarding';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Entypo } from '@expo/vector-icons'; 
+import { FontAwesome } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -16,11 +20,17 @@ const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Search" component={SearchScreen} />
-      <Tab.Screen name='Notification' component={NotificationScreen}/>
-      <Tab.Screen name='Prfile' component={ProfileScreen}/>
+    <Tab.Navigator  screenOptions={{
+      tabBarActiveTintColor: 'purple',
+      tabBarInActiveTintColor:'gray',
+      tabBarShowLabel:false,
+      headerShown:false,
+
+  }}>
+      <Tab.Screen  name="Home" options={{tabBarIcon:(props)=>  <FontAwesome {...props} size={35} name="home"/>}} component={HomeScreen}  />
+      <Tab.Screen name="Search" options={{tabBarIcon:(props)=> <FontAwesome {...props} size={35} name="search"/>}} component={SearchScreen} />
+      <Tab.Screen name='Notification'options={{tabBarIcon:(props)=> <Ionicons {...props} name="notifications-circle" size={40}  />}} component={NotificationScreen}/>
+      <Tab.Screen name='Prfile'options={{tabBarIcon:(props)=> <FontAwesome {...props} size={35} name="user"/>}} component={ProfileScreen}/>
     </Tab.Navigator>
   );
 }
