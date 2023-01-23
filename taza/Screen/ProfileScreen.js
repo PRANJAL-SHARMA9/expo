4d62f6538185fbacd15eb9ff571f1264722c1aa0
PRANJAL-SHARMA9 <97629914+PRANJAL-SHARMA9@users.noreply.github.com>
2023-01-23 07:50:07 +0000

    "working"

---
+++ taza/Screen/ProfileScreen.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { View, Text,Button } from 'react-native';
+
+function ProfileScreen() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+export default ProfileScreen;
